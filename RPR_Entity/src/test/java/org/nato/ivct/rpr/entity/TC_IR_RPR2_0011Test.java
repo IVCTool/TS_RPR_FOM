649cbb06fa0f5ad953beff6666e68c1aeaa48912
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2023-02-14 09:49:50 +0100

    fix tc names

--- RPR_Entity/src/test/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0011Test.java
+++ RPR_Entity/src/test/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0011Test.java
@@ -2,6 +2,7 @@ package org.nato.ivct.rpr.entity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import hla.rti1516e.RTIambassador;
 
@@ -13,16 +14,17 @@ public class TC_IR_RPR2_0011Test {
     @Test
     void testPerformTest() {
         TC_IR_RPR2_0011 tc = new TC_IR_RPR2_0011();
-        tc.setSutFederateName("TC_IR_RPR2_0010");
+        tc.setSutFederateName("TC_IR_RPR2_0011");
         tc.setFederationName("TestFederation");
         tc.setSkipOperatorMsg(true);
         tc.execute(log);
     }
 
     @Test
+    @Disabled
     void testCapabilitiesTest() {
         TC_IR_RPR2_0011 tc = new TC_IR_RPR2_0011();
-        tc.setSutFederateName("TC_IR_RPR2_0010");
+        tc.setSutFederateName("TC_IR_RPR2_0011");
         tc.setFederationName("NETN-ETR_TEST");
         tc.setSkipOperatorMsg(true);
         tc.execute(log);
