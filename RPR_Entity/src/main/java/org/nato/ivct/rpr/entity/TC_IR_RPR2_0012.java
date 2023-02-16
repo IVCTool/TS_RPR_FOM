3c8e71de3e894927eb5954abd35709029b366c7d
brfkar <brf@iitb.fhg.de>
2023-02-16 15:19:12 +0100

    Beginning of writing TC_IR_RPR2_0012

--- RPR_Entity/src/main/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0012.java
+++ RPR_Entity/src/main/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0012.java
@@ -14,11 +14,42 @@ limitations under the License. */
 
 package org.nato.ivct.rpr.entity;
 
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.nato.ivct.rpr.FomFiles;
+import org.nato.ivct.rpr.entity.TC_IR_RPR2_0008.TestCaseAmbassador;
 import org.slf4j.Logger;
 
 import de.fraunhofer.iosb.tc_lib_if.AbstractTestCaseIf;
 import de.fraunhofer.iosb.tc_lib_if.TcFailedIf;
 import de.fraunhofer.iosb.tc_lib_if.TcInconclusiveIf;
+import hla.rti1516e.CallbackModel;
+import hla.rti1516e.FederateAmbassador;
+import hla.rti1516e.FederateHandle;
+import hla.rti1516e.NullFederateAmbassador;
+import hla.rti1516e.ObjectClassHandle;
+import hla.rti1516e.ObjectInstanceHandle;
+import hla.rti1516e.RTIambassador;
+import hla.rti1516e.RtiFactory;
+import hla.rti1516e.RtiFactoryFactory;
+import hla.rti1516e.exceptions.AlreadyConnected;
+import hla.rti1516e.exceptions.CallNotAllowedFromWithinCallback;
+import hla.rti1516e.exceptions.ConnectionFailed;
+import hla.rti1516e.exceptions.CouldNotCreateLogicalTimeFactory;
+import hla.rti1516e.exceptions.CouldNotOpenFDD;
+import hla.rti1516e.exceptions.ErrorReadingFDD;
+import hla.rti1516e.exceptions.FederateAlreadyExecutionMember;
+import hla.rti1516e.exceptions.FederateInternalError;
+import hla.rti1516e.exceptions.FederationExecutionAlreadyExists;
+import hla.rti1516e.exceptions.FederationExecutionDoesNotExist;
+import hla.rti1516e.exceptions.InconsistentFDD;
+import hla.rti1516e.exceptions.InvalidLocalSettingsDesignator;
+import hla.rti1516e.exceptions.NotConnected;
+import hla.rti1516e.exceptions.RTIinternalError;
+import hla.rti1516e.exceptions.RestoreInProgress;
+import hla.rti1516e.exceptions.SaveInProgress;
+import hla.rti1516e.exceptions.UnsupportedCallbackModel;
 
 /**
  * SuT shall not update non-applicable PhysicalEntity Attributes 
@@ -27,20 +58,33 @@ import de.fraunhofer.iosb.tc_lib_if.TcInconclusiveIf;
 
 
 public class TC_IR_RPR2_0012 extends AbstractTestCaseIf {
+	RTIambassador rtiAmbassador = null;
+	FederateAmbassador tcAmbassador = null;
+	Logger logger = null;
 
-	 Logger logger = null;
+	class TestCaseAmbassador extends NullFederateAmbassador {
+		@Override
+		public void discoverObjectInstance(
+				ObjectInstanceHandle theObject,
+				ObjectClassHandle theObjectClass,
+				String objectName) throws FederateInternalError {
+			logger.trace("discoverObjectInstance {}", theObject);
+			//semaphore.release(1);
+		}
+
+		@Override
+		public void discoverObjectInstance(
+				ObjectInstanceHandle theObject, 
+				ObjectClassHandle theObjectClass,
+				String objectName, 
+				FederateHandle producingFederate) throws FederateInternalError {
+			logger.trace("discoverObjectInstance {} with producingFederate {}", theObject, producingFederate);
+			discoverObjectInstance(theObject, theObjectClass, objectName);
+			;
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@Override
 	protected void logTestPurpose(Logger logger) {
 		logger.info("Test Case Purpose: \n"
@@ -53,10 +97,42 @@ public class TC_IR_RPR2_0012 extends AbstractTestCaseIf {
 
 	@Override
 	protected void preambleAction(Logger logger) throws TcInconclusiveIf {
-		// TODO Auto-generated method stub
+		RtiFactory rtiFactory;
+		logger.info("preamble action for test {}", this.getClass().getName());
 
+		try {
+			rtiFactory = RtiFactoryFactory.getRtiFactory();
+			rtiAmbassador = rtiFactory.getRtiAmbassador();
+			tcAmbassador = new TestCaseAmbassador();
+
+			ArrayList<URL> fomList = new FomFiles()
+			.addRPR_BASE().addRPR_Enumerations().addRPR_Foundation()
+			.addRPR_Physical().addRPR_Switches().get();
+
+			rtiAmbassador.connect(tcAmbassador, CallbackModel.HLA_IMMEDIATE);
+
+			// rtiAmbassador.createFederationExecution(federationName, fomList.toArray(new URL[fomList.size()]));
+			try {
+			  rtiAmbassador.createFederationExecution(federationName, fomList.toArray(new URL[fomList.size()]) );
+		    } catch (FederationExecutionAlreadyExists ignored) { }
+			
+			rtiAmbassador.joinFederationExecution(this.getClass().getSimpleName(), federationName, fomList.toArray(new URL[fomList.size()]));
+           
+		} catch (RTIinternalError | ConnectionFailed | InvalidLocalSettingsDesignator | UnsupportedCallbackModel 
+				| AlreadyConnected | CallNotAllowedFromWithinCallback | CouldNotCreateLogicalTimeFactory 
+				| FederationExecutionDoesNotExist | InconsistentFDD | ErrorReadingFDD | CouldNotOpenFDD 
+				| SaveInProgress | RestoreInProgress | FederateAlreadyExecutionMember | NotConnected e) {
+			throw new TcInconclusiveIf(e.getMessage());
+		}
 	}
 
+		/*
+		 *
+		 */
+		
+
+		
+		
 	@Override
 	protected void performTest(Logger logger) throws TcInconclusiveIf, TcFailedIf {
 		// TODO Auto-generated method stub
