8d1283cafed8233731a5cab03836099182b6ba5a
brfkar <brf@iitb.fhg.de>
2023-05-12 14:40:23 +0200

    rearranged and cleared TC_IR_RPR_0012.java

--- RPR_Entity/src/test/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0012Test.java
+++ RPR_Entity/src/test/java/org/nato/ivct/rpr/entity/TC_IR_RPR2_0012Test.java
@@ -3,6 +3,10 @@ package org.nato.ivct.rpr.entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.fraunhofer.iosb.tc_lib_if.IVCT_Verdict;
+
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import hla.rti1516e.RTIambassador;
 
@@ -12,15 +16,26 @@ public class TC_IR_RPR2_0012Test {
     public static final Logger log = LoggerFactory.getLogger(TC_IR_RPR2_0012Test.class);
     RTIambassador rtiAmbassador = null;
 
-  
+    /*
     @Test
     void testPerformTest() {
-        //TC_IR_RPR2_0012 tc = new TC_IR_RPR2_0012();
-        TC_IR_RPR2_0012 tc = new TC_IR_RPR2_0012("Aircraft");
-        //TC_IR_RPR2_0012 tc = new TC_IR_RPR2_0012("AmphibiousVehicle");
+        TC_IR_RPR2_0012 tc = new TC_IR_RPR2_0012();
         tc.setSutFederateName("TC_IR_RPR2_0012");
         tc.setFederationName("TestFederation");
         tc.setSkipOperatorMsg(true);
         tc.execute(log);
+    }  */
+    
+    
+    @Test
+    void testPerformTest() {
+        TC_IR_RPR2_0012 tc = new TC_IR_RPR2_0012();
+        tc.setSutFederateName("TC_IR_RPR2_0012");
+        tc.setFederationName("TestFederation");     
+        tc.setSkipOperatorMsg(true);        
+        IVCT_Verdict result = tc.execute(log);
+        assertTrue(result.verdict == IVCT_Verdict.Verdict.PASSED);
     }
+    
+    
 }
