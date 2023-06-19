1742ecbacade81d83bb01ab565d2e9f805673593
brfkar <brf@iitb.fhg.de>
2023-06-19 14:06:57 +0200

    RPR_Physical Testcase project added

---
+++ RPR_Physical/src/main/java/org/nato/ivct/rpr/physical/RPR_Physical_TestSuite.java
@@ -0,0 +1,36 @@
+/**    Copyright 2022, Reinhard Herzog (Fraunhofer IOSB)
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http: //www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+package org.nato.ivct.rpr.physical;
+
+import de.fraunhofer.iosb.tc_lib.HlaTestSuite;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.slf4j.LoggerFactory;
+
+public class RPR_Physical_TestSuite extends HlaTestSuite {
+
+    public RPR_Physical_TestSuite() throws FileNotFoundException, IOException, ParseException {
+        super();
+    }
+
+    public static final String TEST_SUITE_ID = "RPR-PHY-2_0";
+    JSONObject description;
+
+    public static final org.slf4j.Logger log = LoggerFactory.getLogger(RPR_Physical_TestSuite.class);
+
+}
