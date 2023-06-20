b7c0b67a1eac84abcf050f217b97b2ed7efed5a5
brfkar <brf@iitb.fhg.de>
2023-06-20 14:14:05 +0200

    added Module-Directories RPR_Aggregate RPR_Communication RPR_DistributedEmissionRegeneration

---
+++ RPR_Physical/src/test/java/org/nato/ivct/rpr/physical/RPR_Physical_TestSuiteTest.java
@@ -0,0 +1,28 @@
+package org.nato.ivct.rpr.physical;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+
+import de.fraunhofer.iosb.tc_lib_if.AbstractTestCaseIf;
+
+public class RPR_Physical_TestSuiteTest {
+    @Test
+    void testGetTCase() {
+    	// TODO  add the tests to start here
+    	/*
+        try {
+        	RPR_Physical_TestSuiteTest ts = new RPR_Physical_TestSuiteTest();
+            //AbstractTestCaseIf tc = ts.getTestCase("org.nato.ivct.rpr.entity.TC_IR_RPR2_0008");
+            //assertEquals(tc.getClass().getName(),"org.nato.ivct.rpr.entity.TC_IR_RPR2_0008");
+
+        } catch (IOException | ParseException e) {
+            fail(e);
+        }
+        */
+    }
+}
